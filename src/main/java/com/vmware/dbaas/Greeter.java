b94e25373298c8e8f4230efb37e3922b19da4fc4
Rui Tang <ruit@vmware.com>
2014-07-17 18:20:26 +0800

    Add akka sample code

---
+++ src/main/java/com/vmware/dbaas/Greeter.java
@@ -0,0 +1,21 @@
+package com.vmware.dbaas;
+
+import akka.actor.UntypedActor;
+
+public class Greeter extends UntypedActor {
+
+  public static enum Msg {
+    GREET, DONE;
+  }
+
+  @Override
+  public void onReceive(Object msg) {
+    if (msg == Msg.GREET) {
+      System.out.println("Hello World!");
+      getSender().tell(Msg.DONE, getSelf());
+    } else {
+      unhandled(msg);
+    }
+  }
+
+}
---
+++ src/main/java/com/vmware/dbaas/Main.java
@@ -0,0 +1,8 @@
+package com.vmware.dbaas;
+
+public class Main {
+
+  public static void main(String[] args) {
+    akka.Main.main(new String[] { HelloWorld.class.getName() });
+  }
+}
